596cfb8fe1f7f11276e4b94f79eaf7e6ece55118
adityathok <aditya0thok@gmail.com>
2020-09-05 13:21:05 +0700

    hapus woocomerce

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -19,7 +19,6 @@ if ( ! function_exists( 'mjlah_scripts' ) ) {
 
 		$css_version = $theme_version . '.' . filemtime( get_template_directory() . '/css/theme.min.css' );
 		wp_enqueue_style( 'mjlah-styles', get_template_directory_uri() . '/css/theme.min.css', array(), $css_version );
-		wp_enqueue_style( 'mjlah-woocomerce-styles', get_template_directory_uri() . '/css/woocomerce.min.css', array(), $css_version );
 		wp_enqueue_style( 'mjlah-custom-styles', get_template_directory_uri() . '/css/custom.css', array(), $css_version );
 
 		wp_enqueue_script( 'jquery' );
